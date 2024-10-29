084ea6523d6196e509b9e874f6244643152866be
coblu3 <demirel.akif@hotmail.com>
2024-10-29 15:45:22 +0300

    bug fixes

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -11,7 +11,7 @@ const SideBar = () => {
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
         <div className='flex flex-1 flex-col gap-6'>
-            {sideBarLinks.map((link, index)=>{
+            {sideBarLinks.map((link)=>{
                 const isActive = pathName === link.route || (pathName.startsWith (link.route) && link.route!='/' )
 
                 return <Link href={link.route} key={link.label} className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{
