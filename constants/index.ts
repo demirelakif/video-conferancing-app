96c2557987cf9f0bca3f772d50fe58b90198800a
coblu3 <demirel.akif@hotmail.com>
2024-10-29 15:30:38 +0300

    end course

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,12 @@ export const sideBarLinks = [
     { label: 'Recordings', route: '/recordings', imgUrl:'/icons/video.svg' },
     { label: 'Personal Room', route: '/personal-room', imgUrl:'/icons/add-personal.svg' },
 ]
+
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+]
